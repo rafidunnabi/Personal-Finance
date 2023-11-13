f1fcdb794c9cd4e70c295f7691ae2077bb817113
rafidunnabi <rafidunnabipabon@gmail.com>
2023-11-13 18:03:35 +0600

    Personal-Finance Project setup

---
+++ src/main/java/com/personal/finance/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.personal.finance.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping
+    String getPeople(Model model) {
+        model.addAttribute("something", "this is coming from the controller.");
+        return "homePage";
+    }
+    @GetMapping("/addTransaction")
+    public String addTransactionPage() {
+        return "addTransaction";
+    }
+}
---
+++ src/main/resources/static/script.js
@@ -0,0 +1,7 @@
+function openModal() {
+    document.getElementById('expenseModal').style.display = 'flex';
+}
+
+function closeModal() {
+    document.getElementById('expenseModal').style.display = 'none';
+}
